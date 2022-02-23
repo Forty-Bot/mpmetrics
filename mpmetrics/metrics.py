Forty-Bot/mpmetrics

commit 42614b0f31fc967ebc07a845ab0e2b9548bbff67

metrics: Switch to Dict for holding metrics
This removes the multiprocessing proxy dict from the LabeledCollector
implementation. In particular, this should fix dangling processes when
using uwsgi --master.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

mpmetrics/metrics.py
@@ -6,7 +6,6 @@
 import bisect
 from contextlib import contextmanager
 import itertools
-import multiprocessing
 import threading
 import time
 
@@ -16,7 +15,7 @@ import _mpmetrics
 from .atomic import AtomicUInt64, AtomicDouble
 from .generics import IntType
 from .heap import Heap
-from .types import Box, Double, Array, Struct
+from .types import Box, Dict, Double, Array, Struct
 from .util import classproperty, genmask
 
 @contextmanager
@@ -49,17 +48,15 @@ class Collector:
         self._metric._sample(add_sample)
         yield family
 
-class LabeledCollector:
-    _manager_lock = threading.Lock()
+class LabeledCollector(Struct):
+    _fields_ = {
+        '_shared_lock': _mpmetrics.Lock,
+        '_metrics': Dict,
+    }
 
-    @classproperty
-    def manager(cls):
-        with cls._manager_lock:
-            if not hasattr(cls, '_manager'):
-                cls._manager = multiprocessing.Manager()
-            return cls._manager
+    def __init__(self, mem, metric, name, docs, labelnames, registry, kwargs, heap):
+        super().__init__(mem, heap=heap)
 
-    def __init__(self, metric, name, docs, labelnames, registry, heap, kwargs):
         self._metric = metric
         self._name = name
         self._docs = docs
@@ -79,9 +76,6 @@ class LabeledCollector:
 
         self._lock = threading.Lock()
         self._cache = dict()
-
-        self._shared_lock = Box[_mpmetrics.Lock](self._heap)
-        self._metrics = getattr(registry, 'manager', self.manager).dict()
         registry.register(self)
 
     def _label_values(self, values, labels):
@@ -170,8 +164,8 @@ class CollectorFactory:
         heap = getattr(registry, 'heap', self.heap)
 
         if labelnames:
-            return LabeledCollector(self._metric, name, documentation, labelnames, registry, heap,
-                                    kwargs)
+            return Box[LabeledCollector](heap, self._metric, name, documentation, labelnames,
+                                         registry, kwargs)
         return Collector(self._metric, name, documentation, registry, heap, kwargs)
 
 class Counter(Struct):

tests/metric_test.py
@@ -4,7 +4,6 @@
 # Portions of this file are adapted from prometheus_client
 
 from contextlib import nullcontext
-import multiprocessing
 import pickle
 import random
 import time

commit 0be14f5f41d6b22ea5c1b882494823f799f92c40

metrics: Fix collecting samples without a lock
unlike mpmetrics.Dict, types.Dict is not thread-safe at all without
locking. To dead from it, we always have to take a lock.

Fixes: 42614b0 ("metrics: Switch to Dict for holding metrics")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

mpmetrics/metrics.py
@@ -115,7 +115,11 @@ class LabeledCollector(Struct):
     def collect(self):
         family = self._family()
         with self._lock:
-            metrics = self._cache = dict(self._metrics)
+            with self._shared_lock:
+                for values, metric in self._metrics.items():
+                    if values not in self._cache:
+                        self._cache[values] = metric
+            metrics = self._cache
 
         for labelvalues, metric in metrics.items():
             metric_labels = dict(zip(self._labelnames, labelvalues))
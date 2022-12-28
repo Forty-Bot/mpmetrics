Forty-Bot/mpmetrics

commit 28e50e134c2589efe809b3301afbe9077e29f235

Add support for Enums
This adds support for Enums. They are effectively just Gagues in
implementation. However, the exposition requires a label with the name
of the metric. This requires a bit of rework to get things working
(since the metric name was not previously available to _sample).

Signed-off-by: Sean Anderson <seanga2@gmail.com>

mpmetrics/metrics.py
@@ -46,7 +46,7 @@ class Collector:
         family = self._family()
         def add_sample(suffix, value, labels={}):
             family.add_sample(self._name + suffix, labels, value)
-        self._metric._sample(add_sample)
+        self._metric._sample(add_sample, self._name)
         yield family
 
 class LabeledCollector(Struct):
@@ -73,6 +73,9 @@ class LabeledCollector(Struct):
                 raise ValueError(f"reserved label {label}")
 
             if hasattr(self._metric, 'reserved_labels') and label in self._metric.reserved_labels:
+                raise ValueError(f"reserved label {label}")
+
+            if getattr(self._metric, 'name_is_reserved', False) and label == name:
                 raise ValueError(f"reserved label {label}")
 
         self._lock = threading.Lock()
@@ -125,7 +128,7 @@ class LabeledCollector(Struct):
             metric_labels = dict(zip(self._labelnames, labelvalues))
             def add_sample(suffix, value, labels={}):
                 family.add_sample(self._name + suffix, metric_labels | labels, value)
-            metric._sample(add_sample)
+            metric._sample(add_sample, self._name)
         yield family
 
 class CollectorFactory:
@@ -193,7 +196,7 @@ class Counter(Struct):
    
         self._total.add(amount)
 
-    def _sample(self, add_sample):
+    def _sample(self, add_sample, name):
         add_sample('_total', self._total.get())
         add_sample('_created', self._created.value)
 
@@ -224,7 +227,7 @@ class Gauge(Struct):
     def set(self, amount):
         self._value.set(amount)
 
-    def _sample(self, add_sample):
+    def _sample(self, add_sample, name):
         add_sample('', self._value.get())
 
     def set_to_current_time(self):
@@ -268,7 +271,7 @@ class Summary(Struct):
         data.sum.add(amount)
         data.count.add(1)
 
-    def _sample(self, add_sample):
+    def _sample(self, add_sample, name):
         with self._lock:
             count = self._count.add(1 << 63, raise_on_overflow=False)
             hot = self._data[~count >> 63]
@@ -339,7 +342,7 @@ def _Histogram(__name__, bucket_count):
         data.sum.add(amount)
         data.count.add(1)
 
-    def _sample(self, add_sample):
+    def _sample(self, add_sample, name):
         with self._lock:
             count = self._count.add(1 << 63, raise_on_overflow=False)
             hot = self._data[~count >> 63]
@@ -396,3 +399,24 @@ class _HistogramFactory:
         return histogram(heap, thresholds=thresholds, **kwargs)
 
 Histogram = CollectorFactory(_HistogramFactory())
+
+class Enum(Struct):
+    typ = 'stateset'
+    name_is_reserved = True
+    _fields_ = {
+        '_value': AtomicUInt64,
+    }
+
+    def __init__(self, mem, states, **kwargs):
+        super().__init__(mem)
+        self._states = states
+
+    def state(self, state):
+        self._value.set(self._states.index(state))
+
+    def _sample(self, add_sample, name):
+        val = self._value.get()
+        for i, state in enumerate(self._states):
+            add_sample('', int(i == val), { name: state })
+
+Enum = CollectorFactory(Box[Enum])

tests/metric_test.py
@@ -12,7 +12,7 @@ from hypothesis import given, strategies as st
 from prometheus_client.registry import CollectorRegistry
 import pytest
 
-from mpmetrics.metrics import Counter, Gauge, Summary, Histogram
+from mpmetrics.metrics import Counter, Enum, Gauge, Summary, Histogram
 from mpmetrics.atomic import AtomicUInt64
 
 from .common import heap, parallel, parallels, ParallelLoop
@@ -312,7 +312,6 @@ class TestEnum:
     def labels(self, registry):
         return Enum('el', 'help', ['l'], states=['a', 'b', 'c'], registry=registry)
 
-    @pytest.mark.skip()
     def test_enum(self, enum, registry):
         assert get_sample_value(enum, 'e', {'e': 'a'}) == 1
         assert get_sample_value(enum, 'e', {'e': 'b'}) == 0
@@ -326,17 +325,15 @@ class TestEnum:
         with pytest.raises(ValueError):
             enum.state('d')
 
-        with pytest.raises(ValueError):
+        with pytest.raises(TypeError):
             Enum('e', 'help', registry=registry)
 
-    @pytest.mark.skip()
     def test_labels(self, labels):
         labels.labels('a').state('c')
         assert get_sample_value(labels, 'el', {'l': 'a', 'el': 'a'}) == 0
         assert get_sample_value(labels, 'el', {'l': 'a', 'el': 'b'}) == 0
         assert get_sample_value(labels, 'el', {'l': 'a', 'el': 'c'}) == 1
 
-    @pytest.mark.skip()
     def test_overlapping_labels(self, registry):
         with pytest.raises(ValueError):
             Enum('e', 'help', registry=registry, labelnames=['e'])
@@ -433,10 +430,9 @@ class TestLabelCollector:
         h = Histogram('h_seconds', 'help', [], registry=registry, unit="seconds")
         assert h._name == 'h_seconds'
 
-    @pytest.mark.skip()
     def test_no_units_for_info_enum(self, registry):
-        with pytest.raises(ValueError):
-            Info('foo', 'help', unit="x")
+        #with pytest.raises(ValueError):
+        #    Info('foo', 'help', unit="x")
 
         with pytest.raises(ValueError):
             Enum('foo', 'help', unit="x")

commit 2572ebd31c2de4d9026f441ce0f296a3cf648672

metrics: Fix Enum not unpickling properly
This is effectively the same problem as commit eecafea ("metrics: Fix
pickling LabeledCollector"). Enum has local state which isn't in
_fields_, so we have to handle it separately.

Fixes: 28e50e1 ("Add support for Enums")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

mpmetrics/metrics.py
@@ -474,6 +474,13 @@ class Enum(Struct):
         super().__init__(mem)
         self._states = states
 
+    def _getstate(self):
+        return { 'states': self._states }
+
+    def _setstate(self, mem, heap, states):
+        super()._setstate(mem, heap)
+        self._states = states
+
     def state(self, state):
         self._value.set(self._states.index(state))
 

tests/metric_test.py
@@ -389,6 +389,21 @@ class TestEnum:
         with pytest.raises(ValueError):
             Enum('e', 'help', registry=registry, labelnames=['e'])
 
+    class ConcurrentTest(ParallelLoop):
+        def __init__(self, enum, parallel):
+            super().__init__(parallel)
+            self.enum = enum
+
+        def loop(self, n):
+            self.enum.state(['a', 'b', 'c'][n % 3])
+
+        def final(self):
+            for metric in self.enum.collect():
+                assert sum(sample.value for sample in metric.samples) == 1
+
+    def test_concurrent(self, enum, parallel):
+        self.ConcurrentTest(enum, parallel).run()
+
 class TestLabelCollector:
     @pytest.fixture
     def counter(self, registry):
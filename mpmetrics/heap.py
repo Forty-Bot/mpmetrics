Forty-Bot/mpmetrics

commit 0ac5cf3ee7b5716727b9abb936097d70f04e13df

Add heap
This adds support for a heap which allocates from shared memory. We
follow a very simple sbrk-style allocation strategy. Reclaiming free'd
memory is not supported. We can't map the whole file up front, so
whenever we dereference a block we map the memory it occupies (if it is
not already mapped). The OS chooses where memory is mapped, so we cannot
assume that addresses will be the same in different processes.
Additionally, we cannot guarantee that memory in memory mapped at
different times will be contiguous. This limits us to allocations of at
most map_size.

To make working with boxed resources easier, blocks returned by malloc
contain a reference to the heap they were allocated from. This requires
that the whole heap be picklable. To implement this, we use a named
temporary file so we can reconstruct the heap from just the name of the
file. However, we want to ensure that we don't have multiple heap
objects with the same backing file in the same process, since they will
not share maps. Because there is a limit on the number of memory maps
available to a process, this is undesirable. To address this, we cache
heaps and only create a new heap if there is not one already created for
this file. This is a bit tricky because we need to keep the lock across
either __new__ and __init__.

We allocate cacheline-aligned by default because most of our allocations
are going to be locks or atomic variables, and this will help reduce
contention.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

mpmetrics/heap.py
@@ -1,0 +1,133 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+# Copyright (C) 2021-22 Sean Anderson <seanga2@gmail.com>
+
+import itertools
+import mmap
+import os
+from tempfile import NamedTemporaryFile
+import threading
+from weakref import WeakValueDictionary
+
+import _mpmetrics
+from .types import Size_t, Struct
+from .util import align, _align_check
+
+SC_LEVEL1_DCACHE_LINESIZE = 190
+if (CACHELINESIZE := os.sysconf(SC_LEVEL1_DCACHE_LINESIZE)) < 0:
+    CACHELINESIZE = 64 # Assume 64-byte cache lines
+
+PAGESIZE = mmap.ALLOCATIONGRANULARITY
+
+class Heap(Struct):
+    _fields_ = {
+        '_shared_lock': _mpmetrics.Lock,
+        '_base': Size_t,
+    }
+
+    # Only create one heap per process to avoid duplicate mappings
+    _heaps_lock = threading.Lock()
+    _heaps = WeakValueDictionary()
+
+    def __new__(cls, map_size=PAGESIZE, filename=None):
+        cls._heaps_lock.acquire()
+        if filename:
+            heap = cls._heaps.get(filename, None)
+            if heap:
+                cls._heaps_lock.release()
+                return heap
+
+        try:
+            return super().__new__(cls)
+        except:
+            if filename:
+                cls._heaps_lock.release()
+            raise
+
+    # Must be called with Heap._heaps_lock held; it will be released
+    def __init__(self, map_size=PAGESIZE, filename=None):
+        try:
+            if map_size % PAGESIZE:
+                raise ValueError("size must be a multiple of {}".format(PAGESIZE))
+            _align_check(map_size)
+            self.map_size = map_size
+
+            # File backing our shared memory
+            if filename:
+                self._file = open(filename, 'a+b') 
+            else:
+                self._file = NamedTemporaryFile()
+            self._fd = self._file.fileno()
+
+            if not filename:
+                # Allocate a page to start with
+                os.truncate(self._fd, map_size)
+
+            # Process-local shared memory maps
+            self._maps = [mmap.mmap(self._fd, map_size)]
+            # Lock for _maps
+            self._lock = threading.Lock()
+
+            super().__init__(memoryview(self._maps[0])[:self.size], filename)
+            if not filename:
+                self._base.value = self.size
+
+            # Add ourself to the list of heaps
+            self._heaps[self._file.name] = self
+        finally:
+            self._heaps_lock.release()
+
+    def __getnewargs__(self):
+        return self.map_size, self._file.name
+
+    def __getstate__(self):
+        return self.map_size, self._file.name
+
+    def __setstate__(self, state):
+        map_size, filename = state
+        if not hasattr(self, '_file'):
+            self.__init__(map_size, filename)
+
+    def __del__(self):
+        if hasattr(self, '_file'):
+            self._file.close()
+
+    class Block:
+        def __init__(self, heap, start, size):
+            self.heap = heap
+            self.start = start
+            self.size = size
+
+        def deref(self):
+            heap = self.heap
+            page = int(self.start / heap.map_size)
+            page_off = page * heap.map_size
+            off = self.start - page_off
+            with heap._lock:
+                if len(heap._maps) <= page:
+                    heap._maps.extend(itertools.repeat(None, page - len(heap._maps) + 1))
+                if not self.heap._maps[page]:
+                    heap._maps[page] = mmap.mmap(heap._fd, heap.map_size, offset=page_off)
+                map = heap._maps[page]
+
+            return memoryview(map)[off:off+self.size]
+
+        def free(self):
+            pass
+
+    def malloc(self, size, alignment=CACHELINESIZE):
+        if size <= 0:
+            raise ValueError("size must be strictly positive")
+        elif size > self.map_size:
+            raise ValueError("size must be less than {}".format(self.map_size))
+        _align_check(alignment)
+
+        with self._shared_lock:
+            total = align(self._base.value, self.map_size)
+            self._base.value = align(self._base.value, alignment)
+            if self._base.value + size >= total:
+                os.ftruncate(self._fd, total + self.map_size)
+                self._base.value = total
+            start = self._base.value
+            self._base.value += size
+
+        return self.Block(self, start, size)

mpmetrics/__init__.py
@@ -1,0 +1,2 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>

mpmetrics/types.py
@@ -1,0 +1,55 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+import ctypes
+
+from .util import align, classproperty
+
+def _wrap_ctype(__name__, ctype):
+    size = ctypes.sizeof(ctype)
+    align = ctypes.alignment(ctype)
+
+    def __init__(self, mem, init=True):
+        self._mem = mem
+        self._value = ctype.from_buffer(mem)
+
+    def __getattr__(self, name):
+        return getattr(self.__dict__['_value'], name)
+
+    def __setattr__(self, name, value):
+        if '_value' in self.__dict__:
+            setattr(self.__dict__['_value'], name, value)
+        else:
+            self.__dict__[name] = value
+
+    def __delattr__(self, name):
+        delattr(self.__dict__['_value'], name)
+
+    return type(__name__, (), { name: value for name, value in locals().items()
+                                if name != 'ctype'})
+
+Double = _wrap_ctype('Double', ctypes.c_double)
+Size_t = _wrap_ctype('Size_t', ctypes.c_size_t)
+Int64 = _wrap_ctype('Int64', ctypes.c_int64)
+UInt64 = _wrap_ctype('UInt64', ctypes.c_uint64)
+
+class Struct:
+    def __init__(self, mem, init=True):
+        self._mem = mem
+        off = 0
+        for name, field in self._fields_.items():
+            off = align(off, field.align)
+            setattr(self, name, field(mem[off:off + field.size], init))
+            off += field.size
+            
+    @classproperty
+    def size(cls):
+        size = 0
+        for name, field in cls._fields_.items():
+            size = align(size, field.align)
+            size += field.size
+        return size
+
+    @classproperty
+    def align(cls):
+        return max(field.align for name, field in cls._fields_.items())

mpmetrics/util.py
@@ -1,0 +1,25 @@
+# SPDX-License-Identifier: LGPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+def _align_mask(x, mask):
+    return (x + mask) & ~mask
+
+def _align_check(a):
+    if not a or a & (a - 1):
+        raise ValueError("{} is not a power of 2".format(a))
+
+def align(x, a):
+    _align_check(a)
+    return _align_mask(x, a - 1)
+
+def align_down(x, a):
+    _align_check(a)
+    return x & ~(a - 1)
+
+def genmask(hi, lo):
+    return (-1 << lo) & ~(-1 << (hi + 1))
+
+# From https://stackoverflow.com/a/7864317/5086505
+class classproperty(property):
+    def __get__(self, cls, owner):
+        return classmethod(self.fget).__get__(None, owner)()

tests/common.py
@@ -1,0 +1,83 @@
+# SPDX-License-Identifier: GPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+from collections import namedtuple
+import multiprocessing
+import queue
+import threading
+import time
+
+import pytest
+
+from mpmetrics.heap import Heap
+
+Parallel = namedtuple('Parallel', ('spawn', 'barrier', 'queue'))
+parallels = {
+    'process': Parallel(
+            spawn = multiprocessing.Process,
+            barrier = multiprocessing.Barrier,
+            queue = multiprocessing.Queue
+        ),
+    'thread': Parallel(
+            spawn = threading.Thread,
+            barrier = threading.Barrier,
+            queue = queue.Queue,
+        ),
+}
+
+@pytest.fixture(scope='session', params=('thread', 'process'))
+def parallel(request):
+    return parallels[request.param]
+
+class ParallelLoop:
+    def __init__(self, parallel, n=4, count=None):
+        self.parallel = parallel
+        if parallel.spawn is threading.Thread:
+            n = n or 2
+            count = count or 10000
+        else:
+            n = n or 4
+            count = count or 100000
+        self.n = n
+        self.count = count
+        self.total = n * count
+        self.barrier = self.parallel.barrier(self.n + 1)
+
+    def target(self, n):
+        if hasattr(self, 'setup'):
+            try:
+                self.setup(n)
+            except:
+                self.barrier.abort()
+                raise
+
+        self.barrier.wait()
+        for i in range(self.count):
+            self.loop(i)
+
+    def run(self):
+        procs = [self.parallel.spawn(target=self.target, args=(i,)) for i in range(self.n)]
+        for p in procs:
+            p.start()
+        self.barrier.wait()
+
+        try:
+            if hasattr(self, 'check'):
+                while any(p.is_alive() for p in procs):
+                    self.check()
+                    time.sleep(0)
+        except:
+            for p in procs:
+                if hasattr(p, 'terminate'):
+                    p.terminate()
+            raise
+        finally:
+            for p in procs:
+                p.join()
+
+        if hasattr(self, 'final'):
+            self.final()
+
+@pytest.fixture(scope="session")
+def heap():
+    return Heap()

tests/heap_test.py
@@ -1,0 +1,91 @@
+# SPDX-License-Identifier: GPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+import math
+import mmap
+
+import pytest
+from hypothesis import given, settings, strategies as st
+
+from mpmetrics.heap import Heap
+from mpmetrics.util import align
+
+from .common import parallel
+
+@given(st.integers().filter(lambda n: n % mmap.PAGESIZE))
+def test_bad_map_size(map_size):
+    with pytest.raises(ValueError):
+        Heap(map_size=map_size)
+
+@given(st.integers(max_value=0))
+def test_small_size(size):
+    with pytest.raises(ValueError):
+        Heap().malloc(size)
+
+@given(st.integers(min_value=mmap.PAGESIZE + 1))
+def test_big_size(size):
+    with pytest.raises(ValueError):
+        Heap().malloc(size)
+
+@given(st.integers().filter(lambda a: a <= 0 or math.log2(a) % 1))
+def test_bad_align(alignment):
+    with pytest.raises(ValueError):
+        Heap().malloc(1, alignment)
+
+@st.composite
+def allocs(draw):
+    size = mmap.ALLOCATIONGRANULARITY << draw(st.integers(0, 8))
+    heap = Heap(map_size=size)
+    sizes = st.integers(1, size)
+    aligns = st.integers(0, 12).map(lambda n: 1 << n)
+    return heap, draw(st.lists(st.tuples(sizes, aligns), min_size=3))
+
+@given(allocs())
+@settings(max_examples=25)
+def test_malloc(alloc):
+    h, paramlist = alloc
+
+    blocks = [h.malloc(*params) for params in paramlist]
+    for ((size, alignment), block) in zip(paramlist, blocks):
+        assert block.size == size
+        assert block.start == align(block.start, alignment)
+
+    mems = [block.deref() for block in blocks]
+    for (block, mem) in zip(blocks, mems):
+        assert len(mem) == block.size
+        assert not any(mem)
+        mem[:] = b'A' * len(mem)
+
+    blocks.sort(key=lambda block: block.start)
+    for i in range(len(blocks)):
+        if i:
+            prev = blocks[i - 1]
+            assert prev.start + prev.size <= blocks[i].start
+
+def test_prefork(parallel):
+    mem = Heap().malloc(1).deref()
+    assert mem[0] == 0
+
+    def setone(mem):
+        mem[0] = 1
+
+    p = parallel.spawn(target=setone, args=(mem,))
+    p.start()
+    p.join()
+    assert mem[0] == 1
+
+def test_postfork(parallel):
+    def setone(q):
+        mem = q.get().deref()
+        mem[0] = 2
+
+    q = parallel.queue()
+    p = parallel.spawn(target=setone, args=(q,))
+    p.start()
+
+    h = Heap()
+    h.malloc(mmap.PAGESIZE)
+    block = h.malloc(1)
+    q.put(block)
+    p.join()
+    assert block.deref()[0] == 2

tests/util_test.py
@@ -1,0 +1,36 @@
+# SPDX-License-Identifier: GPL-3.0-only
+# Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
+
+import math
+import mmap
+
+import pytest
+from hypothesis import assume, given, strategies as st
+
+from mpmetrics.util import align, align_down, genmask
+
+@given(st.integers(), st.integers(0, 100).map(lambda n: 1 << n))
+def test_align(x, a):
+    res = align(x, a)
+    assert res % a == 0
+    assert res >= x
+    assert res - a < x
+
+    res = align_down(x, a)
+    assert res % a == 0
+    assert res <= x
+    assert res + a >= x
+
+@given(st.integers(), st.integers(1).filter(lambda a: math.log2(a) % 1))
+def test_npot(x, a):
+    with pytest.raises(ValueError):
+        align(x, a)
+
+    with pytest.raises(ValueError):
+        align_down(x, a)
+
+@given(st.integers(0, 63), st.integers(0, 63))
+def test_genmask(hi, lo):
+    assume(hi >= lo)
+    for bit in range(lo, hi):
+        assert (1 << bit) & genmask(hi, lo)

commit e654adb26e0930550e7420c8613f7ade9ba16168

heap: Properly handle errors in os.sysctl
os.sysctl raises an OSError on failure.

Fixes: 0ac5cf3 ("Add heap")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

mpmetrics/heap.py
@@ -13,7 +13,9 @@ from .types import Size_t, Struct
 from .util import align, _align_check
 
 SC_LEVEL1_DCACHE_LINESIZE = 190
-if (CACHELINESIZE := os.sysconf(SC_LEVEL1_DCACHE_LINESIZE)) < 0:
+try:
+    CACHELINESIZE = os.sysconf(SC_LEVEL1_DCACHE_LINESIZE)
+except OSError:
     CACHELINESIZE = 64 # Assume 64-byte cache lines
 
 PAGESIZE = 4096
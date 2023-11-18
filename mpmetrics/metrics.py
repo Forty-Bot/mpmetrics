Forty-Bot/mpmetrics

commit 78f0ce76d0c5a04b1fb491f90a9bc7c821f4547a

types: Rework state-setting functions
__setstate__ doesn't really implement the copy protocol. What we really
want is to be able to supply a block of memory and tell an object
"initialize yourself". Plus, keyword arguments are nice. We use one to
supply the heap, which will be used by Dict.

This generally shifts more information from the pickled part to the part
being pickled. Since we can't free data we should generally try to store
as much as we can in fixed-size parts of the code.

Signed-off-by: Sean Anderson <seanga2@gmail.com>

mpmetrics/metrics.py
@@ -310,6 +310,7 @@ _HistogramData = IntType('_HistogramData', _HistogramData)
 def _Histogram(__name__, bucket_count):
     typ = 'histogram'
     _fields_ = {
+        '_thresholds': Array[Double, bucket_count],
         '_lock': _mpmetrics.Lock,
         '_data': Array[_HistogramData[bucket_count], 2],
         '_count': AtomicUInt64,
@@ -317,18 +318,16 @@ def _Histogram(__name__, bucket_count):
     }
 
     def __init__(self, mem, thresholds, **kwargs):
+        Struct.__init__(self, mem)
         assert len(thresholds) == bucket_count
         self.thresholds = thresholds
-
-        Struct.__init__(self, mem)
+        for threshold, initial in zip(self._thresholds, thresholds):
+            threshold.value = initial
         self._created.value = time.time()
 
-    def __getstate__(self):
-        return (*Struct.__getstate__(self), self.thresholds)
-
-    def __setstate__(self, state):
-        Struct.__setstate__(self, state[:-1])
-        self.thresholds = state[-1]
+    def _setstate(self, mem, heap):
+        Struct._setstate(self, mem)
+        self.thresholds = tuple(threshold.value for threshold in self._thresholds)
 
     def observe(self, amount, exemplar=None):
         if exemplar is not None:
@@ -392,7 +391,7 @@ class _HistogramFactory:
             thresholds.append(float('inf'))
         if len(thresholds) < 2:
             raise ValueError('must have at least two thresholds')
-        thresholds =  tuple(thresholds)
+        thresholds = tuple(thresholds)
 
         histogram = Box[_Histogram[len(thresholds)]]
         return histogram(heap, thresholds=thresholds, **kwargs)

mpmetrics/types.py
@@ -10,17 +10,14 @@ def _wrap_ctype(__name__, ctype):
     size = ctypes.sizeof(ctype)
     align = ctypes.alignment(ctype)
 
-    def __init__(self, mem):
+    def __init__(self, mem, heap=None):
         self._mem = mem
         self._value = ctype.from_buffer(mem)
         self._value.value = 0
 
-    def __getstate__(self):
-        return (self._mem,)
-
-    def __setstate__(self, state):
-        self._mem = state[0]
-        self._value = ctype.from_buffer(self._mem)
+    def _setstate(self, mem, heap=None):
+        self._mem = mem
+        self._value = ctype.from_buffer(mem)
 
     def __getattr__(self, name):
         return getattr(self.__dict__['_value'], name)
@@ -61,19 +58,16 @@ class Struct:
     def align(cls):
         return max(field.align for field in cls._fields_.values())
 
-    def __init__(self, mem):
+    def __init__(self, mem, heap=None):
         self._mem = mem
         for name, field, off in self._fields_iter():
-            setattr(self, name, field(mem[off:off + field.size]))
+            setattr(self, name, field(mem[off:off + field.size], heap=heap))
 
-    def __getstate__(self):
-        return self._mem, tuple(getattr(self, name).__getstate__()[1:] for name in self._fields_)
-
-    def __setstate__(self, state):
-        self._mem, fields_states = state
-        for (name, field, off), state in zip(self._fields_iter(), fields_states):
+    def _setstate(self, mem, heap=None):
+        self._mem = mem
+        for name, field, off in self._fields_iter():
             field = field.__new__(field)
-            field.__setstate__((self._mem[off:off + field.size], *state))
+            field._setstate(self._mem[off:off + field.size], heap=heap)
             setattr(self, name, field)
 
 def Array(__name__, cls, n):
@@ -83,23 +77,20 @@ def Array(__name__, cls, n):
     member_size = align(cls.size, cls.align)
     size = member_size * n
 
-    def __init__(self, mem):
+    def __init__(self, mem, heap=None):
         self._mem = mem
         self._vals = []
         for i in range(n):
             off = i * member_size
-            self._vals.append(cls(self._mem[off:off + member_size]))
+            self._vals.append(cls(self._mem[off:off + member_size], heap=heap))
 
-    def __getstate__(self):
-        return self._mem, tuple(val.__getstate__()[1:] for val in self._vals)
-
-    def __setstate__(self, state):
-        self._mem, vals_states = state
+    def _setstate(self, mem, heap=None):
+        self._mem = mem
         self._vals = []
-        for i, state in zip(range(n), vals_states):
+        for i in range(n):
             off = i * member_size
             val = cls.__new__(cls)
-            val.__setstate__((self._mem[off:off + member_size], *state))
+            val._setstate(self._mem[off:off + member_size], heap=heap)
             self._vals.append(val)
 
     def __len__(self):
@@ -127,14 +118,14 @@ Array = ProductType('Array', Array, (ObjectType, IntType))
 class _Box:
     def __init__(self, heap, *args, **kwargs):
         block = heap.malloc(self.size)
-        super().__init__(block.deref(), *args, **kwargs)
+        super().__init__(block.deref(), *args, heap=heap, **kwargs)
         self._block = block
 
     def __getstate__(self):
-        return self._block, *super().__getstate__()[1:]
+        return self._block
 
     def __setstate__(self, state):
-        self._block = state[0]
-        super().__setstate__((self._block.deref(), *state[1:]))
+        self._block = state
+        super()._setstate(self._block.deref(), heap=self._block.heap)
 
 Box = ObjectType('Box', lambda name, cls: type(name, (_Box, cls), {}))

_mpmetrics.c
@@ -58,28 +58,19 @@ static void Buffer_dealloc(BufferObject *self)
 	Py_TYPE(self)->tp_free((PyObject *)self);
 }
 
-static PyObject *Buffer_getstate(BufferObject *self, PyObject *Py_UNUSED(ignored))
+static PyObject *Buffer_setstate(BufferObject *self, PyObject *args,
+				 PyObject *kwds)
 {
-	return PyTuple_Pack(1, Py_NewRef(self->shm.obj));
-}
-
-static PyObject *Buffer_setstate(BufferObject *self, PyObject *arg)
-{
-	if (Buffer_init(self, arg, NULL))
+	if (Buffer_init(self, args, kwds))
 		return NULL;
 	Py_RETURN_NONE;
 }
 
 static PyMethodDef Buffer_methods[] = {
 	{
-		.ml_name = "__getstate__",
-		.ml_meth = (PyCFunction)Buffer_getstate,
-		.ml_flags = METH_NOARGS,
-	},
-	{
-		.ml_name = "__setstate__",
+		.ml_name = "_setstate",
 		.ml_meth = (PyCFunction)Buffer_setstate,
-		.ml_flags = METH_O,
+		.ml_flags = METH_VARARGS | METH_KEYWORDS,
 	},
 	{ /* Sentinel */ },
 };

mpmetrics/heap.py
@@ -74,14 +74,14 @@ class Heap(Struct):
         return self.map_size, self._file.name
 
     def __getstate__(self):
-        return self.map_size, self._file.name, super().__getstate__()[1:]
+        return self.map_size, self._file.name
 
     def __setstate__(self, state):
         try:
             if hasattr(self, '_file'):
                 return
 
-            self.map_size, filename, super_state = state
+            self.map_size, filename = state
             self._file = open(filename, 'a+b')
             self._fd = self._file.fileno()
 
@@ -90,7 +90,7 @@ class Heap(Struct):
             # Lock for _maps
             self._lock = threading.Lock()
 
-            super().__setstate__((memoryview(self._maps[0])[:self.size], *super_state))
+            super()._setstate(memoryview(self._maps[0])[:self.size])
 
             # Add ourself to the list of heaps
             self._heaps[self._file.name] = self

commit a2f0255c7654da042f77b2cb49b6e32428d62967

Fix histograms not being unpickled correctly
_Histogram._setstate does not pass heap to Struct._setstate, which caused
it to be initialized to None. This results in exemplars breaking, since
they access heap. Fix this, and update the unpickling test to be more
functional.

Fixes: 78f0ce7 ("types: Rework state-setting functions")
Signed-off-by: Sean Anderson <seanga2@gmail.com>

mpmetrics/metrics.py
@@ -386,7 +386,7 @@ def _Histogram(__name__, bucket_count):
         self._created.value = time.time()
 
     def _setstate(self, mem, heap):
-        Struct._setstate(self, mem)
+        Struct._setstate(self, mem, heap)
         self.thresholds = tuple(threshold.value for threshold in self._thresholds)
 
     def observe(self, amount, exemplar=None):

mpmetrics/types.py
@@ -150,6 +150,7 @@ class Object(Struct):
         self._heap = heap
 
     def _setstate(self, mem, heap):
+        assert heap is not None
         super()._setstate(mem, heap)
         self._heap = heap
 

tests/metric_test.py
@@ -349,7 +349,8 @@ def test_time(registry, cls, name):
 @pytest.mark.parametrize('cls', (Counter, Gauge, Summary, Histogram))
 def test_pickle(registry, cls):
     metric = cls('name', 'help', labelnames=('l'), registry=registry)
-    pickle.loads(pickle.dumps(metric.labels('x')))
+    metric.labels('x')
+    assert list(metric.collect()) == list(pickle.loads(pickle.dumps(metric)).collect())
 
 class TestEnum:
     @pytest.fixture
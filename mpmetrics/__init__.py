a9fde253acbc73ec876a793bc1b327476bd771f0
Sean Anderson <seanga2@gmail.com>
2023-11-19 14:02:08 -0500

    Export Enum at the top level
    Enum was never exported from mpmetrics, but it should be. Add it.
    
    Fixes: 28e50e1 ("Add support for Enums")
    Signed-off-by: Sean Anderson <seanga2@gmail.com>

--- mpmetrics/__init__.py
+++ mpmetrics/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: LGPL-3.0-only
 # Copyright (C) 2022 Sean Anderson <seanga2@gmail.com>
 
-from .metrics import Counter, Gauge, Summary, Histogram
+from .metrics import Counter, Gauge, Summary, Histogram, Enum
 
-__all__ = ('Counter', 'Gauge', 'Summary', 'Histogram')
+__all__ = ('Counter', 'Gauge', 'Summary', 'Histogram', 'Enum')
--- mpmetrics/flask.py
+++ mpmetrics/flask.py
@@ -3,17 +3,19 @@
 
 import prometheus_flask_exporter
 
-from . import Gauge, Counter, Summary, Histogram
+from . import Counter, Gauge, Summary, Histogram, Enum
 
 prometheus_flask_exporter.Gauge = Gauge
 prometheus_flask_exporter.Counter = Counter
 prometheus_flask_exporter.Summary = Summary
 prometheus_flask_exporter.Histogram = Histogram
+prometheus_flask_exporter.Enum = Enum
 
 del Gauge
 del Counter
 del Summary
 del Histogram
+del Enum
 
 del prometheus_flask_exporter
 
